4d72a09930ac94efccd20f271b2862fb86138888
thr3a <thr3a@example.com>
2025-04-24 00:28:30 +0900

    fix:

--- src/context-script.ts
+++ src/context-script.ts
@@ -1,9 +1,10 @@
 import browser from 'webextension-polyfill';
+import type { Runtime } from 'webextension-polyfill';
 import type { SendMessageWithValue } from './types';
 
 console.log('content script');
 
-browser.runtime.onMessage.addListener(async (msg, sender): Promise<void> => {
-  const request = msg as SendMessageWithValue<string>;
+browser.runtime.onMessage.addListener((msg: unknown, _sender: Runtime.MessageSender) => {
+  const request = msg as SendMessageWithValue<unknown>;
   console.log('action', request.action, 'value', request.value);
 });
